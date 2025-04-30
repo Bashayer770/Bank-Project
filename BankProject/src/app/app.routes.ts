5f8fc129acf98acec6428dc938189ae0f7a30adf
Bashayer770 <Beshow95@gmail.com>
2025-04-30 12:28:19 +0300

    Files Added

--- BankProject/src/app/app.routes.ts
+++ BankProject/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuardService] },
+  { path: '**', redirectTo: 'login' },
+];
